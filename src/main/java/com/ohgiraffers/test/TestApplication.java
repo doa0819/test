a4be0d7afc6bdc83e770b57bebc7fc469fd5ac05
doa <leedoa0819@gmail.com>
2024-01-31 14:30:51 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,32 @@
+package com.ohgiraffers.test;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class TestApplication {
+
+    // 수업목표
+
+    // 여기는 주석입니다.(//는 한글로 주석 할 시 사용 -> //은 주석할때 사용)
+    /* 여기는
+    * 여러줄
+    * 주석입니다.
+    * */
+
+    /*
+    * ctrl - space : 자동완성 기능
+    * ctrl - d : 복사
+    * -Dconsole.encoding=UTF-8 : 한글파일 깨질시 새로운 파일 만들어서 다시 치고 재부팅
+    * 한글파일 계속 깨질시 flie - invalidate caches - 다 선택 후 캐시다 삭제
+    * {},//,(),[] : 짝안 맞으면 오류 발생
+    *
+    * */
+
+/*main -> class 시작
+* sout -> shift+f10
+* */
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+
+    }
+}
